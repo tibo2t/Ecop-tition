c8d243268013a7f674bd1796328c667fa40ed54d
tibo2t <thibault@jubert.pro>
2025-03-10 16:25:31 +0100

    Ajout du point d'api d'enregistrement utilisateur

--- Ecopétition/urls.py
+++ Ecopétition/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import RegisterAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/user/register', RegisterAPIView.as_view(), name='register'),
 ]
