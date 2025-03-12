491354a71c68e2523b64b7ccbf4dbb674f25e347
tibo2t <155970326+tibo2t@users.noreply.github.com>
2025-03-12 16:48:50 +0100

    Update settings.py

--- Ecopétition/settings.py
+++ Ecopétition/settings.py
@@ -28,7 +28,13 @@ SECRET_KEY = 'django-insecure-ci68t*nea-p4e8jfr+9-g(97tnc$9yz7l8vfojr!#qmnuibd2w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["app-20ce8ab4-2f87-49c9-a647-2a5fbcdfacbc.cleverapps.io",]
+ALLOWED_HOSTS = ["app-20ce8ab4-2f87-49c9-a647-2a5fbcdfacbc.cleverapps.io",
+                 'localhost',
+                 '127.0.0.1',
+                 'http://localhost:3000',
+                 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
