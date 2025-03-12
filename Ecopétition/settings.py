4a51d2869aadec3210d4e5b883aed09a3f04815c
tibo2t <thibault@jubert.pro>
2025-03-12 11:44:30 +0100

    fix cors erreur

--- Ecopétition/settings.py
+++ Ecopétition/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = 'django-insecure-ci68t*nea-p4e8jfr+9-g(97tnc$9yz7l8vfojr!#qmnuibd2w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["app-20ce8ab4-2f87-49c9-a647-2a5fbcdfacbc.cleverapps.io",
+                 'localhost',
+                 '127.0.0.1',
+                 'localhost:3000', 
+                 ]
 
 
 # Application definition
