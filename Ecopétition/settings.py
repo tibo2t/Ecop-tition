cf18b4d0863502ff6a4cadf3cf67473daf9a04b7
clementgueganviault <clementgueganviault@MacBook-Pro.local>
2025-03-13 11:32:15 +0100

    J'ai mis la variable append slashe False

--- Ecopétition/settings.py
+++ Ecopétition/settings.py
@@ -154,6 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+APPEND_SLASH = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
